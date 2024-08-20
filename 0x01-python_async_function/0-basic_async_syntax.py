7ddda0da1487809b29454ace99d9ecd2d34b2601
Godwin Bruce <godwin.bruce@gmail.com>
2024-08-20 00:22:21 +0000

    async updated

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-""" Module documentation """
+"""define asyncio"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """doc func"""
+    """a random delays btn 0 and max_delay"""
     delay = random.uniform(0, max_delay)
+
+    """wait for the delay"""
     await asyncio.sleep(delay)
+
+    """return delay"""
     return delay
